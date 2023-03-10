b834c777868c3ffc4fab9cdd6e9a13f137d879ed
mscyb12345 <mscyb12345@gmail.com>
2023-03-10 12:29:14 +0900

    file2

---
+++ file.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+ FILE *file;
+ file =fopen("test.txt","r");
+ printf("%c\n",fgetc(file));
+ fclose(file);
+}
---
+++ file2.c
@@ -0,0 +1,13 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+int main(){
+ int fd;
+ char pathname[]="temp.txt";
+ if((fd=open(pathname, O_CREAT | O_RDWR ,0644))== -1){
+  printf("Error\n");
+  exit(1);
+ }
+ close(fd);
+}
