f2550aff75535dc8e603b09922a911f7f3f68839
mscyb12345 <mscyb12345@gmail.com>
2023-03-24 12:22:55 +0900

    2023/03/24

---
+++ ex03-05.c
@@ -0,0 +1,11 @@
+#include<sys/types.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<stdio.h>
+int main(){
+ int filedes;
+ off_t newpos;
+ filedes=open("data1.txt",O_RDONLY);
+ newpos=lseek(filedes,(off_t)0,SEEK_END);
+ printf("file size : %ld\n",newpos);
+}
